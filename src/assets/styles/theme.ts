60a6c8974eee19fee927b4e723d5d979c6de8b86
Bong <2021bongwonny@gmail.com>
2022-12-20 23:37:21 +0900

    Add: theme 컬러 추가

--- src/assets/styles/theme.ts
+++ src/assets/styles/theme.ts
@@ -1,6 +1,23 @@
 import { DefaultTheme } from 'styled-components';
 
-const colors = {};
+const colors = {
+  p_dark: '#1B395F',
+  p_mid: '#79A8D6',
+  p_light: '#C5D7E5',
+
+  luck_gr: '#A3D4B6',
+  luck_r: '#F9A997',
+  luck_bl: '#9FDFEA',
+  luck_p: '#F1C5E1',
+  luck_g: '#F4D8A2',
+
+  border: '#EEEEEE',
+  text_title_bk: '#000000',
+  text_title_wh: '#FFFFFF',
+  text_body_900: '#212121',
+  text_body_700: '#616161',
+  text_system: '#9E9E9E',
+};
 const fonts = {
   title: 'SANJUGotgam',
   system: 'NanumSquareNeo-Variable',
