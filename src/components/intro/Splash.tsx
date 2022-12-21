99d523715ea8be614dd3605f98077da91af9feac
Bong <2021bongwonny@gmail.com>
2022-12-22 00:49:50 +0900

    Add: Splash 컴포넌트 추가 (#4)

---
+++ src/components/intro/Splash.tsx
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+const Splash = () => {
+  return (
+    <Container>
+      <h1 className='title-1 c-wt'>올해까치</h1>
+    </Container>
+  );
+};
+
+export default Splash;
+
+const Container = styled.div`
+  position: absolute;
+  left: 50%;
+  min-height: 100vh;
+  width: 100%;
+  max-width: 540px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: ${({ theme }) => theme.colors.p_mid};
+  transform: translate(-50%);
+`;
