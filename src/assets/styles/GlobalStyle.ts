37ae2d34d573ed0db9e992155fea9bc9d7756f0c
hrim <104122566+hrimwk@users.noreply.github.com>
2022-12-21 14:09:24 +0900

    Add: global className , ArrowBack component (#1)

--- src/assets/styles/GlobalStyle.ts
+++ src/assets/styles/GlobalStyle.ts
@@ -28,7 +28,7 @@ const GlobalStyle = createGlobalStyle`
         padding: 0;
         margin: 0;
         -ms-overflow-style: none;
-    };
+    }
     ::-webkit-scrollbar{
         display: none;
     }
@@ -37,18 +37,75 @@ const GlobalStyle = createGlobalStyle`
         cursor: pointer;
         outline: none;
         border-radius: 3px;
-    };
+    }
+    img{
+        width:100%;
+    }
     input{
         display: flex;
-        outline: none;
         padding-left: 10px;
+        border:1px solid #EEEEEE;
+        border-radius: 4px;
+        outline: none;
     }
     .container{
         margin: 0 auto;
-        padding: 10px;
+        padding: 24px 20px;
         min-height: 100vh;
-        max-width: 540px;
+        max-width: 540px; 
 
+    }
+    .c-bk{
+        color:#000000;
+    }
+    .c-wt{
+        color:#ffffff;
+    }
+    .c-gy-900{
+        color:#212121;
+    }
+    .c-gy-700{
+        color:#616161;
+    }
+    .c-gy-500{
+        color:#9E9E9E;
+    }
+    .title-1{
+        font-family: 'SANJUGotgam';
+        font-size: 24px;
+        line-height: 36px;
+    }
+    .title-2{
+        font-family: 'SANJUGotgam';
+        font-size: 18px;
+        line-height: 28px;
+    }
+    .sub-title-1-eb{
+        font-family: 'NanumSquareNeo-Variable';
+        font-weight: 800;
+        font-size: 14px;
+        line-height: 22px;
+    }
+    .sub-title-1{
+        font-family: 'NanumSquareNeo-Variable';
+        font-size: 14px;
+        line-height: 22px;
+    }
+    .sub-title-2{
+        font-family: 'NanumSquareNeo-Variable';
+        font-weight: 800;
+        font-size: 10x;
+        line-height: 16px;
+    }
+    .body-txt-1{
+        font-family: 'NanumSquareNeo-Variable';
+        font-size: 12px;
+        line-height: 18px;
+    }
+    .body-txt-2{
+        font-family: 'NanumSquareNeo-Variable';
+        font-size: 10px;
+        line-height: 16px;
     }
 `;
 
---
+++ src/components/common/ArrowBack.tsx
@@ -0,0 +1,23 @@
+import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+import arrow_back from '../../assets/images/arrow_back.png';
+
+function ArrowBack() {
+  const navigate = useNavigate();
+  const onClick = () => {
+    navigate(-1);
+  };
+  return (
+    <ArrowBackContainer onClick={onClick}>
+      <img src={arrow_back} />
+    </ArrowBackContainer>
+  );
+}
+const ArrowBackContainer = styled.div`
+  width: 44px;
+  height: 44px;
+  padding: 10px;
+  cursor: pointer;
+`;
+
+export default ArrowBack;
