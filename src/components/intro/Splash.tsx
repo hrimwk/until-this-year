4f5cbf7002b836ec475f2b2b65b3b8a4d5f9308a
hrim <104122566+hrimwk@users.noreply.github.com>
2022-12-24 18:52:16 +0900

    Add: splash animation (#11)

--- src/components/intro/Splash.tsx
+++ src/components/intro/Splash.tsx
@@ -11,8 +11,9 @@ const Splash = () => {
 export default Splash;
 
 const Container = styled.div`
-  position: absolute;
+  position: fixed;
   left: 50%;
+  top: 50%;
   min-height: 100vh;
   width: 100%;
   max-width: 540px;
@@ -22,4 +23,23 @@ const Container = styled.div`
   background-color: ${({ theme }) => theme.colors.p_mid};
   transform: translate(-50%);
   z-index: 999;
+  overflow: hidden;
+  transform: translate(-50%, -50%);
+  .title-1 {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    animation: moveTitle 0.6s 0.4s cubic-bezier(0.6, -0.28, 0.735, 0.02) forwards;
+  }
+  @keyframes moveTitle {
+    0% {
+      transform: scale(1);
+    }
+    30% {
+      transform: scale(1.1);
+    }
+    100% {
+      transform: scale(1);
+    }
+  }
 `;
