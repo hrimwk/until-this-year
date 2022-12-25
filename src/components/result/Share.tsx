2ca34f6673080f3344e524bc986917696813245f
hrim <104122566+hrimwk@users.noreply.github.com>
2022-12-26 01:27:07 +0900

    Add share (#14)
    * Add: share UI & twitter,facebook share
    
    * Add: kakao share
    
    * Fix: add share component in result page

---
+++ src/components/result/Share.tsx
@@ -0,0 +1,69 @@
+import { useEffect } from 'react';
+import styled from 'styled-components';
+import { FacebookShareButton, TwitterShareButton } from 'react-share';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+
+import facebook from '../../assets/images/facebook.png';
+import twitter from '../../assets/images/twitter.png';
+import kakao from '../../assets/images/kakao.png';
+import url from '../../assets/images/url.png';
+import download from '../../assets/images/download.png';
+
+declare global {
+  interface Window {
+    Kakao: any;
+  }
+}
+function Share() {
+  const currentUrl = window.location.href;
+  useEffect(() => {
+    if (window.Kakao) {
+      const kakao = window.Kakao;
+      if (!kakao.isInitialized()) {
+        kakao.init(import.meta.env.VITE_REACT_APP_KAKAO_KEY);
+      }
+    }
+  }, []);
+  const shareKakao = () => {
+    window.Kakao.Share.sendScrap({
+      requestUrl: 'http://172.30.1.20:5173',
+      templateId: 87708,
+    });
+  };
+
+  return (
+    <ShareContainer>
+      <div className='img-wrap' onClick={shareKakao}>
+        <img src={kakao} alt='kakao' />
+      </div>
+      <FacebookShareButton url={currentUrl}>
+        <div className='img-wrap'>
+          <img src={facebook} alt='facebook' />
+        </div>
+      </FacebookShareButton>
+      <TwitterShareButton url={currentUrl}>
+        <div className='img-wrap'>
+          <img src={twitter} alt='twitter' />
+        </div>
+      </TwitterShareButton>
+      <CopyToClipboard text={currentUrl} onCopy={() => alert('클립보드에 복사되었습니다.')}>
+        <div className='img-wrap'>
+          <img src={url} alt='url' />
+        </div>
+      </CopyToClipboard>
+      <div className='img-wrap'>
+        <img src={download} alt='download' />
+      </div>
+    </ShareContainer>
+  );
+}
+const ShareContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  .img-wrap {
+    width: 44px;
+    cursor: pointer;
+  }
+`;
+export default Share;
