55f08e2b2c50abb37f0e480cf9321aa7f2a3eb8a
hrim <hrim.wk@gmail.com>
2022-12-26 01:22:30 +0900

    Add: kakao share

--- src/components/result/Share.tsx
+++ src/components/result/Share.tsx
@@ -1,16 +1,39 @@
+import { useEffect } from 'react';
 import styled from 'styled-components';
+import { FacebookShareButton, TwitterShareButton } from 'react-share';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+
 import facebook from '../../assets/images/facebook.png';
 import twitter from '../../assets/images/twitter.png';
 import kakao from '../../assets/images/kakao.png';
 import url from '../../assets/images/url.png';
 import download from '../../assets/images/download.png';
-import { FacebookShareButton, FacebookIcon, TwitterIcon, TwitterShareButton } from 'react-share';
 
+declare global {
+  interface Window {
+    Kakao: any;
+  }
+}
 function Share() {
   const currentUrl = window.location.href;
+  useEffect(() => {
+    if (window.Kakao) {
+      const kakao = window.Kakao;
+      if (!kakao.isInitialized()) {
+        kakao.init(import.meta.env.VITE_REACT_APP_KAKAO_KEY);
+      }
+    }
+  }, []);
+  const shareKakao = () => {
+    window.Kakao.Share.sendScrap({
+      requestUrl: 'http://172.30.1.20:5173',
+      templateId: 87708,
+    });
+  };
+
   return (
     <ShareContainer>
-      <div className='img-wrap'>
+      <div className='img-wrap' onClick={shareKakao}>
         <img src={kakao} alt='kakao' />
       </div>
       <FacebookShareButton url={currentUrl}>
@@ -23,9 +46,11 @@ function Share() {
           <img src={twitter} alt='twitter' />
         </div>
       </TwitterShareButton>
-      <div className='img-wrap'>
-        <img src={url} alt='url' />
-      </div>
+      <CopyToClipboard text={currentUrl} onCopy={() => alert('클립보드에 복사되었습니다.')}>
+        <div className='img-wrap'>
+          <img src={url} alt='url' />
+        </div>
+      </CopyToClipboard>
       <div className='img-wrap'>
         <img src={download} alt='download' />
       </div>
@@ -38,6 +63,7 @@ const ShareContainer = styled.div`
   width: 100%;
   .img-wrap {
     width: 44px;
+    cursor: pointer;
   }
 `;
 export default Share;
