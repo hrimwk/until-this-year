3746918a2218752a9100e128aeaa331c32583673
hrim <104122566+hrimwk@users.noreply.github.com>
2023-02-04 21:03:20 +0900

    Fix: mock data로 이메일 유무 판단 (#47)
    * Fix: mock data로 이메일 유무 판단

--- src/pages/UserAuth.tsx
+++ src/pages/UserAuth.tsx
@@ -33,21 +33,13 @@ function UserAuth() {
   const handleChecked = () => setConsentCheck((prev) => !prev);
 
   const handleGoNextStep = () => {
-    axios
-      .post(import.meta.env.VITE_SERVER_EMAIL_CHECK_URL, { email })
-      .then((res) => {
-        if (res.data.message === 'Available') {
-          navigator('/fortune');
-        }
-      })
-      .catch((err) => {
-        if (err.response.status === 409) {
-          setModal(true);
-          return;
-        }
-        alert('에러가 발생했습니다. 잠시 후 다시 시도해주세요.');
-        console.log(err);
-      });
+    axios.get('../../db.json').then((res) => {
+      if (res.data.users.filter((user: { email: string }) => user.email === email).length > 0) {
+        setModal(true);
+      } else {
+        navigator('/fortune');
+      }
+    });
   };
 
   return (
